38b5ba0fd16c6e3c6870ac2116741783ff1d5f82
jcmaria <jcmaria@mapfre.com.br>
2023-07-07 14:38:17 -0300

    first commit

---
+++ src/main/java/com/fastsw/impresao/ImpresaoApplication.java
@@ -0,0 +1,13 @@
+package com.fastsw.impresao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpresaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpresaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastsw/impresao/ImpresaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastsw.impresao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpresaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
